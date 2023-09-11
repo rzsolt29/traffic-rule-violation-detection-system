69cec33a63f56b45e92ef7afdc1ed1e89a870517
Révész Zsolt <revzsolt29@gmail.com>
2023-09-11 11:45:10 +0200

    Sávok megkülönböztetése

--- findLanes.py
+++ findLanes.py
@@ -3,8 +3,6 @@ import numpy as np
 
 background = cv2.imread("result_pictures/newVideoSource/blured.png")
 
-#cv2.imshow("background", background)
-
 # polynom creation for masking
 stencil = np.zeros_like(background[:, :, 0])
 polygon = np.array([[80, 80], [250, 80], [500, 650], [-100, 650]])
@@ -17,18 +15,12 @@ masked = cv2.bitwise_and(background[:, :, 0], background[:, :, 0], mask=stencil)
 ret, thresh = cv2.threshold(masked, 130, 145, cv2.THRESH_BINARY)
 #cv2.imshow("thresh", thresh)
 
-####
-####
 
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
 close = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=5)
 
 cv2.imshow("closed", close)
 
-####
-####
-
-###########
 
 contours, _ = cv2.findContours(close, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
@@ -45,10 +37,10 @@ biggests = [sortedData[0][1], sortedData[1][1]]
 
 x, y, w, h = cv2.boundingRect(biggests[0])
 cv2.drawContours(close, biggests[0], -1, (255, 0, 0), 1)
-cv2.rectangle(close, (x, y), (x+w, y+h), (0,255,0), 1)
+cv2.rectangle(close, (x, y), (x+w, y+h), (0, 255, 0), 1)
 x, y, w, h = cv2.boundingRect(biggests[1])
 cv2.drawContours(close, biggests[1], -1, (255, 0, 0), 1)
-cv2.rectangle(close, (x, y), (x+w, y+h), (0,255,0), 1)
+cv2.rectangle(close, (x, y), (x+w, y+h), (0, 255, 0), 1)
 cv2.imshow("before", close)
 after = cv2.cvtColor(close, cv2.COLOR_GRAY2BGRA)
 
@@ -83,8 +75,34 @@ for i in range(81, after.shape[0] - 1):
             after[i][j][1] = 0
             after[i][j][2] = 0
 
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
-after = cv2.morphologyEx(after, cv2.MORPH_CLOSE, kernel, iterations=4)
+
+after = cv2.cvtColor(after, cv2.COLOR_BGR2GRAY)
+
+laneChange = []
+for i in range(81, after.shape[0] - 1):
+    colorChangedOnce = False
+    firstLaneReady = False
+    for j in range(after.shape[1] - 2):
+
+        if not colorChangedOnce and after[i][j] != after[i][j+1]:
+            colorChangedOnce = True
+        elif colorChangedOnce and after[i][j] != 0 and after[i][j+1] == 0:
+            firstLaneReady = True
+            laneChange.append(j)
+            break
+        elif colorChangedOnce and not firstLaneReady:
+            after[i][j] = 255
+
+for i in range(1, len(laneChange)-82):
+    print(f'{laneChange[i-1]} > {laneChange[i]*1.1}')
+    if laneChange[i-1] > laneChange[i]*1.1:
+        print("done")
+        after[i+80]=after[i+81]
+        laneChange[i - 1] = laneChange[i]
+    if laneChange[i]*1.2 < laneChange[i+1]:
+        after[i + 82] = after[i + 81]
+        laneChange[i + 1] = laneChange[i]
+
 
 """# makes blue the second half of the road
 for i in range(81, after.shape[0] - 1):
@@ -103,80 +121,6 @@ for i in range(81, after.shape[0] - 1):
 
 
 cv2.imshow("after", after)
-
-###########
-# trying skeletonization
-"""img = thresh.copy()
-
-size = np.size(img)
-skel = np.zeros(img.shape, np.uint8)
-
-ret, img = cv2.threshold(img, 127, 255, 0)
-element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
-done = False
-
-while (not done):
-    eroded = cv2.erode(img, element)
-    temp = cv2.dilate(eroded, element)
-    temp = cv2.subtract(img, temp)
-    skel = cv2.bitwise_or(skel, temp)
-    img = eroded.copy()
-
-    zeros = size - cv2.countNonZero(img)
-    if zeros == size:
-        done = True
-
-cv2.imshow("skel", skel)
-
-# Filter using contour area and remove small noise
-cnts = cv2.findContours(skel, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-for c in cnts:
-    area = cv2.contourArea(c)
-    if area < 5500:
-        cv2.drawContours(thresh, [c], -1, (0,0,0), -1)
-
-# Morph close
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
-close = cv2.morphologyEx(skel, cv2.MORPH_CLOSE, kernel, iterations=2)
-
-
-cv2.imshow('close', close)
-
-
-# Canny edges detection and morph close 
-edges = cv2.Canny(thresh, 100, 200)
-cv2.imshow("edges", edges)
-closedEdges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel, iterations=2)
-cv2.imshow("closed edges", closedEdges)
-
-
-
-
-closedEdgesEdges = cv2.Canny(closedEdges, 100, 200)
-cv2.imshow("closed edges edges", closedEdgesEdges)
-
-#####
-size = np.size(closedEdges)
-closedSkel = np.zeros(closedEdges.shape, np.uint8)
-
-ret, closedEdges = cv2.threshold(closedEdges, 127, 255, 0)
-element = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
-done = False
-
-while not done:
-    eroded = cv2.erode(closedEdges, element)
-    temp = cv2.dilate(eroded, element)
-    temp = cv2.subtract(closedEdges, temp)
-    closedSkel = cv2.bitwise_or(closedSkel, temp)
-    closedEdges = eroded.copy()
-
-    zeros = size - cv2.countNonZero(closedEdges)
-    if zeros == size:
-        done = True
-
-cv2.imshow("closedSkel", closedSkel)"""
-
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
