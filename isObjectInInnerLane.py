1a86e9c22428826f2398bb1eb5f5b5c99d859489
Révész Zsolt <revzsolt29@gmail.com>
2023-10-31 17:28:27 +0100

    Függvény kidolgozása

--- isObjectInInnerLane.py
+++ isObjectInInnerLane.py
@@ -1,2 +1,16 @@
+import cv2
+
+
 def is_object_in_inner_lane(y, height, x, width, lanes):
-    return False
+    x_center = x+width//2
+    y_center = y+height//2
+
+    if (lanes[y][x] == 255).any():
+        return False
+    else:
+        return True
+
+
+if __name__ == "__main__":
+    lanes = cv2.imread("result_pictures/newVideoSource/laneLocalizationFullSize.png")
+    print(is_object_in_inner_lane(3800, 10, 20, 45, lanes))
