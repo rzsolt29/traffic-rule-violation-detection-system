04f680d90854281031a73fca90a62dc631ad7dfd
Révész Zsolt <revzsolt29@gmail.com>
2023-11-11 15:07:31 +0100

    Logger használata

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import sys
 import datetime
 import time
+import logging
 
 from database_operations.createTable import create_table
 from dto.measuringPlace import MeasuringPlace
 from keepBackground import keep_background
 from findLanes import find_lanes
 from movingObjectCutter import moving_object_cutter
+
+logging.basicConfig(level=logging.INFO)
 
 if len(sys.argv) == 2:
     VIDEO_PATH = sys.argv[1]
@@ -22,26 +25,30 @@ latitude = input("Geographic coordinates (latitude)")
 longitude = input("Geographic coordinates (longitude)")
 measuring_place = MeasuringPlace(name_of_road, kilometric_point, direction, latitude, longitude)
 
+if input(f"Is the local time {datetime.datetime.now().replace(microsecond=0)}? (y/n)") == 'n':
+    logging.error("SYSTEM TIME ERROR")
+    exit()
+
 create_table()
-print("Database table created")
+logging.info("Database table created")
 
 try:
     background = keep_background(VIDEO_PATH)
 except BaseException:
-    print("Cannot open camera. Camera index out of range")
+    logging.error("Cannot open camera. Camera index out of range")
     exit()
 
-print("Background created")
+logging.info("Background created")
 
 lanes = find_lanes(background)
-print("Lanes found")
+logging.info("Lanes found")
 
 while True:
     if 6 < datetime.datetime.now().hour < 22:
         try:
             moving_object_cutter(VIDEO_PATH, lanes, measuring_place)
         except BaseException:
-            print("Cannot open camera. Camera index out of range")
+            logging.error("Cannot open camera. Camera index out of range")
             exit()
     else:
         time.sleep(120)
