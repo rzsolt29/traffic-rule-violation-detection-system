a5a90d9ef79e297330143ee256a50fe01d699360
Révész Zsolt <revzsolt29@gmail.com>
2023-11-22 14:04:29 +0100

    Bugfix

--- isObjectInInnerLane.py
+++ isObjectInInnerLane.py
@@ -2,15 +2,22 @@ import cv2
 
 
 def is_object_in_inner_lane(y, height, x, width, lanes):
+
     x_center = x+width//2
     y_center = y+height//2
 
-    if (lanes[y][x] == 255).any():
-        return False
-    else:
+    if lanes[y][x] == 76:
         return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
     lanes = cv2.imread("result_pictures/newVideoSource/laneLocalizationFullSize.png")
-    print(is_object_in_inner_lane(3800, 10, 20, 45, lanes))
+    lanes = lanes[:, :, 0]
+
+    # outer lane test
+    print(is_object_in_inner_lane(3819, 10, 32, 52, lanes))
+
+    # inner lane test
+    #print(is_object_in_inner_lane(1170, 10, 1170, 52, lanes))
