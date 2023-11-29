30a41177f740ae1f9ed8ea5349b01e2776619ad8
Révész Zsolt <revzsolt29@gmail.com>
2023-11-29 16:08:41 +0100

    Refact és módosított kép elnevezés

--- database_operations/addViolation.py
+++ database_operations/addViolation.py
@@ -6,17 +6,22 @@ from dto.measuringPlace import MeasuringPlace
 
 
 def add_violation(image, measuring_place):
+    dt = datetime.datetime.now()
+
+    if 6 > dt.hour > 22:
+        return
+
     with psycopg.connect("host="+os.environ['HOST']+" port="+os.environ['PORT']+" dbname="+os.environ['DBNAME']+" user="+os.environ['USER']+" password="+os.environ['PASSWORD']) as conn:
         with conn.cursor() as cur:
 
-            dt = datetime.datetime.now()
-
-            if 6 > dt.hour > 22:
-                return
-
             ts = datetime.datetime.timestamp(dt)
 
-            img_path = "D:\\Dev\\Szakdoga\\traffic-rule-violation-detection-system\\violations\\"+str(ts).replace(".", "")+".jpg"
+            img_path = "D:\\Dev\\Szakdoga\\traffic-rule-violation-detection-system\\violations\\"+\
+                       measuring_place.name_of_road +\
+                       str(measuring_place.kilometric_point) +\
+                       measuring_place.direction[:4] +\
+                       str(ts).replace(".", "") +\
+                       ".jpg"
 
             cv2.imwrite(img_path, image)
 
@@ -31,5 +36,5 @@ def add_violation(image, measuring_place):
 
 if __name__ == "__main__":
     img = cv2.imread("D:\\Dev\\Szakdoga\\traffic-rule-violation-detection-system\\test_img.png")
-    measuring_place = MeasuringPlace("M44", 153, "Békéscsaba", 46.740207099204696, 20.818157445847874)
+    measuring_place = MeasuringPlace("M44", 153, "Bekescsaba", 46.740207099204696, 20.818157445847874)
     add_violation(img, measuring_place)
